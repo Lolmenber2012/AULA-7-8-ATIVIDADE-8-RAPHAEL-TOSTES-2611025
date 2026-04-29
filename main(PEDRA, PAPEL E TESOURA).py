b1b49fd31a58783828f4e8c5db17ec63a63f3138
Raphael <raphaeltostes2006@gmail.com>
2026-04-29 11:59:28 -0300

    Aula 2

--- main(PEDRA, PAPEL E TESOURA).py
+++ main(PEDRA, PAPEL E TESOURA).py
@@ -1,1 +1,46 @@
 import random
+vitoriaDoJogador=0
+vitoriaDoPC=0
+
+def jogadas():
+  jogador = input("Entre sua escolha (pedra, papel, tesoura): ")
+  opções = ["pedra", "papel","tesoura"]
+  computador = random.choice(opções)
+  escolhas = {"Jogador": jogador, "computador":computador}
+  return escolhas
+
+def verificarVitoria(jg, pc):
+  print(f"Você: {jg}. Computador: {pc}.")
+  if jg == pc:
+   return "EMPATE!" 
+  elif jg == "pedra":
+    if pc == "tesoura":
+      
+      return "pedra derrota tesoura! FATALITY!"
+    else:
+      
+      return "papel cobre pedra. Se FU***."
+  elif  jg == "papel":
+    if pc == "pedra":
+      
+      return "papel cobre pedra! FATALITY!"
+    else:
+      
+      return "papel foi triturado pelas tesouras. Se FU***."    
+  elif jg == "tesoura":
+    if pc == "papel":
+      
+      return "tesoura tritura o papel! FATALITY!"
+  else:
+    
+    return "tesouras foi esmagada pela pedra. Se FU***."
+   
+escolhas = jogadas()
+result = verificarVitoria(escolhas["Jogador"], escolhas["computador"])
+print(result)
+print(f'Pontuação:\
+    Jogador:{vitoriaDoJogador} PC:{vitoriaDoPC}')
+
+
+
+  
